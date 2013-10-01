0f89ac744158dfceb2bed87bc79bf9ec3ea539b9
Brian Ray <brian.ray1@gmail.com>
2013-10-01 18:46:33 -0400

    Use lazy let for unsaved objects

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,11 +1,9 @@
 require "test_helper"
 
 describe User do
-  before do
-    @user = User.new
-  end
+  let(:user) { User.new }
 
   it "must be valid" do
-    @user.valid?.must_equal true
+    user.valid?.must_equal true
   end
 end
