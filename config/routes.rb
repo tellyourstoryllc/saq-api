b0e648071c3c267f424f89bb9ea38187ac1ba196
Brian Ray <brian.ray1@gmail.com>
2013-10-04 13:02:41 -0400

    Changing to non-RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 ChatApp::Application.routes.draw do
-  resources :users
-  get '/join/:join_code', to: 'groups#join', as: :join_group
+  scope via: :all do
+    match '/users/create', to: 'users#create', as: 'create_user'
+    match '/users/update', to: 'users#update', as: 'update_user'
 
-  resources :groups
-  post '/user/update', to: 'users#update', as: 'update_user'
+    match '/login', to: 'sessions#create', as: 'login'
 
-  post '/login', to: 'sessions#create', as: 'login'
+    match '/groups/create', to: 'groups#create', as: 'create_group'
+    match '/join/:join_code', to: 'groups#join', as: 'join_group'
+  end
 end
