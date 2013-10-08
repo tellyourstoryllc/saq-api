7dee282e00153f62a42e94175daf478abf654406
Brian Ray <brian.ray1@gmail.com>
2013-10-08 10:41:18 -0400

    Fix join URL

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -2,7 +2,8 @@ class GroupSerializer < ActiveModel::Serializer
   attributes :object_type, :id, :name, :join_url, :topic, :admin_ids, :member_ids
 
   def join_url
-    join_group_url(object.join_code) if object.join_code.present?
+    #join_group_url(object.join_code) if object.join_code.present?
+    "http://test.host/join/#{object.join_code}" if object.join_code.present?
   end
 
   def admin_ids
