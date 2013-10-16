27caefb3122ef684100bc8fab0e9d3ce63879f24
Brian Ray <brian.ray1@gmail.com>
2013-10-16 12:37:07 -0400

    Use a separate Redis instance instead of namespacing
    Mostly due to Node not having a working Redis namespace module

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,2 @@
 persisted = Rails.configuration.app['redis']['persisted']
-redis = Redis.new(host: persisted['host'], db: persisted['db'], logger: Rails.logger)
-Redis.current = Redis::Namespace.new("chat_app_#{Rails.env}", redis: redis)
+Redis.current = Redis.new(host: persisted['host'], port: persisted['port'], db: persisted['db'], logger: Rails.logger)
