7633c7ce0d0a2dba204179d2fe1479f993f91090
Brian Ray <brian.ray1@gmail.com>
2013-10-31 14:07:48 -0400

    Publish a new message to the room if need be
    Messages with an attachment are sent directly to
    the API and need to be published to the room

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,6 +10,16 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
 
     if @message.save
+      unless params[:skip_publish]
+        endpoint = URI.parse(Rails.configuration.app['faye']['url'])
+        message = {
+          channel: "/groups/#{@group.id}/messages",
+          data: MessageSerializer.new(@message).as_json,
+          ext: {token: params[:token], persisted: true}
+        }
+        Net::HTTP.post_form(endpoint, message: message.to_json)
+      end
+
       # Notify all Idle/Unavailable mentioned members
       @message.mentioned_users.each do |recipient|
         MessageMailer.mention(@message, recipient, recipient.computed_status).deliver! if recipient.idle_or_unavailable?
