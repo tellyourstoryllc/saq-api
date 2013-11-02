c5a9b962da6fd60223a6a72bb96cb50763e018a0
Cragin Godley <cgodley@gmail.com>
2013-11-01 20:08:59 -0400

    Emoticon: make (philosoraptor) higher contrast

--- app/models/emoticon.rb
+++ app/models/emoticon.rb
@@ -4,7 +4,7 @@ class Emoticon < ActiveRecord::Base
   validates :name, :image_data, presence: true
   scope :active, -> { where(active: true) }
 
-  VERSION = 4
+  VERSION = 5
 
 
   def self.by_version(version)
