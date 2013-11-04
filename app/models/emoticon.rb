4a1eaf4552e92a7c07e131db16185c144e2f10ec
Cragin Godley <cgodley@gmail.com>
2013-11-04 11:28:01 -0500

    Emoticon: (notsureif)

--- app/models/emoticon.rb
+++ app/models/emoticon.rb
@@ -4,7 +4,7 @@ class Emoticon < ActiveRecord::Base
   validates :name, :image_data, presence: true
   scope :active, -> { where(active: true) }
 
-  VERSION = 5
+  VERSION = 6 
 
 
   def self.by_version(version)
