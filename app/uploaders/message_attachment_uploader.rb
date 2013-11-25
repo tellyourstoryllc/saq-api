8d505c9ce48f67c4dccd08eb50ca104a9b91be47
Brian Ray <brian.ray1@gmail.com>
2013-11-25 18:17:56 -0500

    Need it here too

--- app/uploaders/message_attachment_uploader.rb
+++ app/uploaders/message_attachment_uploader.rb
@@ -7,6 +7,10 @@ class MessageAttachmentUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   include CarrierWave::MimeTypes
 
+  def cache_dir
+    '/mnt/rails/skymob-cache'
+  end
+
   # Choose what kind of storage to use for this uploader:
   # storage :file
   storage :fog
