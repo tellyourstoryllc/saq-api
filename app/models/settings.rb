27fdb34986a7052f20eaa72bd465f85f9c540367
Brian Ray <brian.ray1@gmail.com>
2013-12-11 18:24:20 -0500

    Global app settings

---
+++ app/models/settings.rb
@@ -0,0 +1,32 @@
+class Settings
+  include Redis::Objects
+
+  hash_key :attrs, global: true
+  FEATURES = [:queue]
+
+
+  def self.get(key)
+    val = attrs[key]
+    val == '_nil' ? nil : val
+  end
+
+  def self.set(key, value)
+    attrs[key] = value.nil? ? '_nil' : value
+  end
+
+  def self.feature_toggle_key(feature)
+    "feature_#{feature}"
+  end
+
+  def self.enabled?(feature)
+    get(feature_toggle_key(feature)) == '1'
+  end
+
+  def self.enable!(feature)
+    set(feature_toggle_key(feature), '1')
+  end
+
+  def self.disable!(feature)
+    set(feature_toggle_key(feature), '0')
+  end
+end
