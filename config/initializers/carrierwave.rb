92dd3452259f8b25cea049f3cf934ea959f8b7e0
Brian Ray <brian.ray1@gmail.com>
2013-12-23 11:25:30 -0500

    Fix SSL bug introduced in newer version of Fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,8 @@ CarrierWave.configure do |config|
     :region                 => 'us-west-2',                     # optional, defaults to 'us-east-1'
     #:host                   => 's3.example.com',                # optional, defaults to nil
     #:endpoint               => 'https://s3.example.com:8080'    # optional, defaults to nil
-    :endpoint               => 'https://s3.amazonaws.com'       # optional, defaults to nil
+    :endpoint               => 'https://s3.amazonaws.com',       # optional, defaults to nil
+    :path_style             => true
   }
   config.fog_directory  = aws_config['bucket_name']               # required
   config.fog_public     = true                                    # optional, defaults to true
