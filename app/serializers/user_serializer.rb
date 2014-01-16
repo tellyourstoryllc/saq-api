197d2b6080d948e4ac64ce8e0a0f71f2ecef91e6
Brian Ray <brian.ray1@gmail.com>
2014-01-16 14:43:30 -0500

    Include everything when viewing yourself

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,7 +2,7 @@ class UserSerializer < ActiveModel::Serializer
   attributes :object_type, :id, :token, :name, :username, :status, :status_text, :idle_duration, :client_type, :avatar_url
 
   def status
-    if current_user && object.contact?(current_user)
+    if contacts?
       object.computed_status
     else
       'unavailable'
@@ -10,15 +10,15 @@ class UserSerializer < ActiveModel::Serializer
   end
 
   def status_text
-    object.status_text if current_user && object.contact?(current_user)
+    object.status_text if contacts?
   end
 
   def idle_duration
-    object.idle_duration if current_user && object.contact?(current_user)
+    object.idle_duration if contacts?
   end
 
   def client_type
-    if current_user && object.contact?(current_user)
+    if contacts?
       object.computed_client_type
     else
       'web'
@@ -27,5 +27,12 @@ class UserSerializer < ActiveModel::Serializer
 
   def include_token?
     respond_to?(:current_user) && current_user.try(:id) == id
+  end
+
+
+  private
+
+  def contacts?
+    current_user && (current_user.id == object.id || object.contact?(current_user))
   end
 end
