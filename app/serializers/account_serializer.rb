e4032ee3e5f5bfa54440faa657e33dde2ff5025d
Brian Ray <brian.ray1@gmail.com>
2014-01-20 16:06:25 -0500

    Include a flag for if the account needs a password

--- app/serializers/account_serializer.rb
+++ app/serializers/account_serializer.rb
@@ -1,7 +1,15 @@
 class AccountSerializer < ActiveModel::Serializer
-  attributes :object_type, :id, :user_id, :one_to_one_wallpaper_url, :facebook_id, :time_zone
+  attributes :object_type, :id, :user_id, :one_to_one_wallpaper_url, :facebook_id, :time_zone, :needs_password
 
   def include_facebook_id?
     current_user.try(:id) == object.user_id
+  end
+
+  def include_needs_password?
+    object.no_login_credentials?
+  end
+
+  def needs_password
+    object.no_login_credentials?
   end
 end
