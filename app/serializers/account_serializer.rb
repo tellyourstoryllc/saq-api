f3abe777f503ac95c1d3e35b6d4f2944942e6699
Brian Ray <brian.ray1@gmail.com>
2014-01-28 14:44:41 -0500

    Further restrict account serializer

--- app/serializers/account_serializer.rb
+++ app/serializers/account_serializer.rb
@@ -1,15 +1,27 @@
 class AccountSerializer < ActiveModel::Serializer
   attributes :object_type, :id, :user_id, :one_to_one_wallpaper_url, :facebook_id, :time_zone, :needs_password
 
+
   def include_facebook_id?
-    current_user.try(:id) == object.user_id
+    owner?
+  end
+
+  def include_time_zone?
+    owner?
   end
 
   def include_needs_password?
-    object.no_login_credentials?
+    owner? && object.no_login_credentials?
   end
 
   def needs_password
     object.no_login_credentials?
+  end
+
+
+  private
+
+  def owner?
+    respond_to?(:current_user) && current_user.try(:id) == object.user_id
   end
 end
