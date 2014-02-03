19bc8f8ed4cb00d6723d0a00fe1e1fafcb74b172
Brian Ray <brian.ray1@gmail.com>
2014-02-03 18:12:08 -0500

    Per-group email digest prefs

--- app/serializers/user_group_preferences_serializer.rb
+++ app/serializers/user_group_preferences_serializer.rb
@@ -1,3 +1,3 @@
 class UserGroupPreferencesSerializer < ActiveModel::Serializer
-  attributes :object_type, :id, :user_id, :group_id, :server_all_messages_mobile_push
+  attributes :object_type, :id, :user_id, :group_id, :server_all_messages_mobile_push, :server_all_messages_email
 end
