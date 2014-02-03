7e110a4a960bd5faea8a2e71d9e0a235ade783ac
Brian Ray <brian.ray1@gmail.com>
2014-02-03 10:10:02 -0500

    Add "(file attached)" to email notifications

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -1,5 +1,6 @@
 class BaseMailer < ActionMailer::Base
   default from: 'skymob <info@skymob.co>'
+  add_template_helper ApplicationHelper
 
   def mail(options)
     super unless options[:to].blank?
