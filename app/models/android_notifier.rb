72006a4fdf0bdd8208ce1294741372482615f70e
Brian Ray <brian.ray1@gmail.com>
2014-02-06 15:13:36 -0500

    Add custom data in the right place

--- app/models/android_notifier.rb
+++ app/models/android_notifier.rb
@@ -1,7 +1,7 @@
 class AndroidNotifier < MobileNotifier
   def build_notification(alert, custom_data = {})
     app = Rails.configuration.app['rapns']['android_app']
-    attrs = custom_data.merge(app: app, data: {message: alert})
+    attrs = {app: app, data: custom_data.merge(message: alert)}
     notification = Rapns::Apns::Notification.new(attrs)
 
     Rails.logger.debug "Notification: #{notification.inspect}; payload_size: #{notification.payload_size}"
