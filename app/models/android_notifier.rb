ccadab0b9522d9a3eb081900bd8d28ac32bcdda8
Brian Ray <brian.ray1@gmail.com>
2014-02-11 17:35:42 -0500

    Fix GCM notifications

--- app/models/android_notifier.rb
+++ app/models/android_notifier.rb
@@ -2,7 +2,7 @@ class AndroidNotifier < MobileNotifier
   def build_notification(alert, custom_data = {})
     app = Rails.configuration.app['rapns']['android_app']
     attrs = {app: app, data: custom_data.merge(message: alert)}
-    notification = Rapns::Apns::Notification.new(attrs)
+    notification = Rapns::Gcm::Notification.new(attrs)
 
     Rails.logger.debug "Notification: #{notification.inspect}; payload_size: #{notification.payload_size}"
     notification
