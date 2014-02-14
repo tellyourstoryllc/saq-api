a1fe439b063e4a34acad2503e4bee6958d5731d5
Brian Ray <brian.ray1@gmail.com>
2014-02-14 15:58:38 -0500

    Revert this migration change

--- db/migrate/20131220144240_change_some_cols_to_utf8mb4.rb
+++ db/migrate/20131220144240_change_some_cols_to_utf8mb4.rb
@@ -1,13 +1,13 @@
 class ChangeSomeColsToUtf8mb4 < ActiveRecord::Migration
   def up
     execute 'ALTER TABLE groups MODIFY topic VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL'
-    execute 'ALTER TABLE rpush_notifications MODIFY alert VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL'
+    execute 'ALTER TABLE rapns_notifications MODIFY alert VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL'
     execute 'ALTER TABLE users MODIFY status_text VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL'
   end
 
   def down
     execute 'ALTER TABLE users MODIFY status_text VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL'
-    execute 'ALTER TABLE rpush_notifications MODIFY alert VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL'
+    execute 'ALTER TABLE rapns_notifications MODIFY alert VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL'
     execute 'ALTER TABLE groups MODIFY topic VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL'
   end
 end
