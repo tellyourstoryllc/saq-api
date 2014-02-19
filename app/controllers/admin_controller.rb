cfe51ce246a5038a4bfbef496cd54b94b3bcce90
Brian Ray <brian.ray1@gmail.com>
2014-02-19 18:27:31 -0500

    SMS stats admin interface

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,15 @@
+class AdminController < ActionController::Base
+  http_basic_authenticate_with name: Rails.configuration.app['admin']['username'], password: Rails.configuration.app['admin']['password']
+
+  def sms_stats
+    @today = Time.zone.today
+
+    @daily_sent_sms_counts = HookClient.daily_sent_sms_counts.all
+    @monthly_sent_sms_counts = HookClient.monthly_sent_sms_counts.all
+    @all_time_sent_sms_count = HookClient.all_time_sent_sms_count.get
+
+    @daily_received_sms_counts = HookClient.daily_received_sms_counts.all
+    @monthly_received_sms_counts = HookClient.monthly_received_sms_counts.all
+    @all_time_received_sms_count = HookClient.all_time_received_sms_count.get
+  end
+end
