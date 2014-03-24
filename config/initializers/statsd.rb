4d48e16aed0eab06e295f56deb08a568ea4fd399
Brian Ray <brian.ray1@gmail.com>
2014-03-24 16:30:46 -0400

    Setting to make StatsD prefix different than name

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -4,6 +4,6 @@ StatsD.server = statsd['server']
 StatsD.logger = Rails.logger
 StatsD.mode = :production
 
-prefix = Rails.configuration.app['app_name'].dup.split(' ').map(&:underscore).join('_')
+prefix = Rails.configuration.app['statsd']['prefix']
 prefix << "_#{Rails.env}" unless Rails.env.production?
 StatsD.prefix = prefix
