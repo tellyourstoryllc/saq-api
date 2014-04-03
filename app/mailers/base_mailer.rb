568858cde92320cb1d491029a327018fcd691b3c
Brian Ray <brian.ray1@gmail.com>
2014-04-03 16:48:25 -0400

    Don't email fake email addresses

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -2,8 +2,9 @@ class BaseMailer < ActionMailer::Base
   default from: "#{Rails.configuration.app['app_name']} <info@#{Rails.configuration.app['sendgrid']['domain']}>"
   include ActionView::Helpers::DateHelper
   add_template_helper ApplicationHelper
+  BLACKLISTED_DOMAINS = /ffm.fm|krazykam.com/
 
   def mail(options)
-    super unless options[:to].blank?
+    super unless options[:to].blank? || options[:to] =~ BLACKLISTED_DOMAINS
   end
 end
