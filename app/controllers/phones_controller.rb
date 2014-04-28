857d4b1a4e56f80b50f04ef96ec5a7c9f688b21f
Brian Ray <brian.ray1@gmail.com>
2014-04-28 12:22:18 -0400

    Fix phone verification

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,17 +1,29 @@
 class PhonesController < ApplicationController
-  before_action :load_phone
-
-
   def create
-    if @phone.save
-      HookClient.send_verification(@phone.number, @phone.verification_code) unless @phone.verified?
-      render_json current_user
+    number = Phone.normalize(params[:phone_number])
+    render_error and return if number.blank?
+
+    @phone = Phone.find_or_initialize_by(number: number)
+
+    if @phone.persisted?
+      send_verification if @phone.user_id == current_user.id
     else
-      render_error
+      @phone.user = current_user
+      @phone.save!
+
+      send_verification
     end
+
+    render_json current_user
   end
 
   def verify
+    number = Phone.normalize(params[:phone_number])
+    render_error and return if number.blank?
+
+    @phone = Phone.find_by(number: number)
+    render_error and return if @phone.nil?
+
     if @phone.verify_by_code!(current_user, params[:phone_verification_code], {notify_friends: true})
       mixpanel.verified_phone(@phone, :entered_phone)
       render_json current_user
@@ -23,8 +35,7 @@ class PhonesController < ApplicationController
 
   private
 
-  def load_phone
-    number = Phone.normalize(params[:phone_number])
-    @phone = Phone.find_or_initialize_by(number: number) if number
+  def send_verification
+    HookClient.send_verification(@phone.number, @phone.verification_code) unless @phone.verified?
   end
 end
