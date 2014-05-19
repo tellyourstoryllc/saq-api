d59199550983a56d9f29e548f24c45f7b396cc1b
Brian Ray <brian.ray1@gmail.com>
2014-05-19 15:34:07 -0400

    Fix tests

--- test/factories/snap_invite_ads.rb
+++ test/factories/snap_invite_ads.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :snap_invite_ad do
     name 'Features'
-    image_url 'http://example.com/features.png'
+    media_url 'http://example.com/features.png'
     text_overlay 'Check out our cool features'
   end
 end
