ed4598d469f453d8e55af07b7ee86cfa39fe97e5
Brian Ray <brian.ray1@gmail.com>
2014-05-19 16:53:10 -0400

    Add snap.io to blacklisted email domains

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -2,7 +2,7 @@ class BaseMailer < ActionMailer::Base
   default from: "#{Rails.configuration.app['app_name']} <info@#{Rails.configuration.app['sendgrid']['domain']}>"
   include ActionView::Helpers::DateHelper
   add_template_helper ApplicationHelper
-  BLACKLISTED_DOMAINS = /ffm.fm|krazykam.com/
+  BLACKLISTED_DOMAINS = /snap.io|ffm.fm|krazychat.com|krazykam.com/
 
   def mail(options)
     super unless options[:to].blank? || options[:to] =~ BLACKLISTED_DOMAINS
