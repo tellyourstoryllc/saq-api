1262ad2ca6823610f76bb8ec7942dccd23e163be
Brian Ray <brian.ray1@gmail.com>
2014-05-19 18:34:34 -0400

    Replace variable in robot items

--- app/models/robot_item.rb
+++ app/models/robot_item.rb
@@ -3,6 +3,18 @@ class RobotItem < ActiveRecord::Base
     trigger.split(',')
   end
 
+  def text
+    if self[:text].include?('%max_help_number%')
+      self[:text].gsub('%max_help_number%', self.class.max_help_number)
+    else
+      self[:text]
+    end
+  end
+
+  def self.max_help_number
+    valid_triggers.select{ |t| t =~ /^\d+$/ }.max{ |t| t.to_i }
+  end
+
   def self.by_trigger(trigger)
     where("FIND_IN_SET(?, `trigger`)", trigger).order(:rank)
   end
