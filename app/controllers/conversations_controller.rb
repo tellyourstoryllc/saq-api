9d1d0598715d3c21456935c41e7c66149226c19c
Jonathan Tran <jonathan@perceptualnet.com>
2014-05-22 18:16:39 -0400

    Changed to not return intro-only conversations for the robot user.

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -4,6 +4,15 @@ class ConversationsController < ApplicationController
 
     groups = current_user.groups.includes(:group_avatar_image, :group_wallpaper_image)
     one_to_ones = current_user.one_to_ones
+
+    # Special case the robot user.
+    if Robot.bot?(current_user)
+      # Don't return conversations with only the intro messages sent by the
+      # robot.
+      num_intro_messages = RobotItem.by_trigger('intro').count
+      one_to_ones = one_to_ones.select{ |o| o.rank.get > num_intro_messages }
+    end
+
     conversations = groups + one_to_ones
     conversations.each{ |convo| convo.viewer = current_user }
     other_users = User.includes(:avatar_image, :avatar_video).where(id: one_to_ones.map{ |o| o.other_user_id(current_user) })
