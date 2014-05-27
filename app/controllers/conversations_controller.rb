479eb89ff20e10325c657e8ba8ce79f9a8cfc941
Brian Ray <brian.ray1@gmail.com>
2014-05-27 17:06:54 -0400

    Also eager-load account

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -31,7 +31,7 @@ class ConversationsController < ApplicationController
 
     conversations.each{ |convo| convo.viewer = current_user }
     other_user_ids = conversations.map{ |c| c.other_user_id(current_user) if c.respond_to?(:other_user_id) }.compact
-    other_users = User.includes(:avatar_image, :avatar_video).where(id: other_user_ids)
+    other_users = User.includes(:account, :avatar_image, :avatar_video).where(id: other_user_ids)
 
     render_json conversations + other_users
   end
