8a34ee7b54cd0157b865949efb67a5267583bab9
Brian Ray <brian.ray1@gmail.com>
2014-05-30 15:10:04 -0400

    Use the API url for resetting admin passwords

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,6 +1,6 @@
 class AdminMailer < BaseMailer
   def password_reset(sysop)
-    @url = admin_reset_password_url(admin_token: sysop.token)
+    @url = admin_reset_password_url(host: Rails.configuration.app['api']['url'], admin_token: sysop.token)
     @name = sysop.name
     mail(to: sysop.email, subject: 'Admin Password Reset Instructions')
   end
