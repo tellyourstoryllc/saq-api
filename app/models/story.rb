7c745b926514a49f0f8b689a60ba8db30f4ac373
Brian Ray <brian.ray1@gmail.com>
2014-06-10 12:14:00 -0400

    A Story's rank is different depending on the list

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,4 +3,20 @@ class Story < Message
     super
     self.type = 'story'
   end
+
+  def rank; end
+
+
+  private
+
+  # Atomically set the rank and add it to the conversation's message list
+  def add_to_conversation
+    convo = conversation
+    return if convo.nil?
+
+    # Add the story to the list, but don't set the rank on the story itself,
+    # since the rank can change depending on the list it's being viewed in
+    lua_script = %{local rank = redis.call('INCR', KEYS[1]); redis.call('ZADD', KEYS[2], rank, ARGV[1])}
+    redis.eval lua_script, {keys: [convo.rank.key, convo.message_ids.key], argv: [id]}
+  end
 end
