c46c562e0667cd47e46c36f3534016b0ddf309bb
Brian Ray <brian.ray1@gmail.com>
2014-06-20 14:28:16 -0400

    Get and set pref to disable comments on my stories

--- app/models/user_preferences.rb
+++ app/models/user_preferences.rb
@@ -2,8 +2,8 @@ class UserPreferences
   include Peanut::RedisModel
   include Redis::Objects
 
-  attr_accessor :id, :user_id, :client_web, :server_mention_email,
-    :server_one_to_one_email, :server_story_privacy, :created_at
+  attr_accessor :id, :user_id, :client_web, :server_mention_email, :server_one_to_one_email,
+    :server_story_privacy, :server_disable_story_comments, :created_at
   hash_key :attrs
   set :server_story_friends_to_block
 
@@ -12,7 +12,8 @@ class UserPreferences
   DEFAULTS = {
     server_one_to_one_email: true,
     server_mention_email: true,
-    server_story_privacy: 'friends'
+    server_story_privacy: 'friends',
+    server_disable_story_comments: false
   }
 
   STORY_PRIVACIES = %w(everyone friends custom)
@@ -24,7 +25,7 @@ class UserPreferences
     if id.present?
       self.user_id = id
       to_int(:id, :created_at)
-      to_bool(:server_mention_email, :server_one_to_one_email)
+      to_bool(:server_mention_email, :server_one_to_one_email, :server_disable_story_comments)
     end
   end
 
@@ -57,15 +58,20 @@ class UserPreferences
     end
   end
 
+  def server_disable_story_comments
+    !@server_disable_story_comments.nil? ? @server_disable_story_comments : DEFAULTS[:server_disable_story_comments]
+  end
+
 
   private
 
   def write_attrs
-    to_bool(:server_mention_email, :server_one_to_one_email)
+    to_bool(:server_mention_email, :server_one_to_one_email, :server_disable_story_comments)
     self.created_at ||= Time.current.to_i
 
     self.attrs.bulk_set(user_id: user_id, client_web: client_web, server_mention_email: server_mention_email,
                         server_one_to_one_email: server_one_to_one_email,
-                        server_story_privacy: server_story_privacy, created_at: created_at)
+                        server_story_privacy: server_story_privacy,
+                        server_disable_story_comments: server_disable_story_comments, created_at: created_at)
   end
 end
