db33e819cdde575b210e5c7cec04d2616469cfcd
Brian Ray <brian.ray1@gmail.com>
2014-07-10 12:20:16 -0400

    Increment stories fetched count metrics

--- app/controllers/snaps_controller.rb
+++ app/controllers/snaps_controller.rb
@@ -3,6 +3,7 @@ class SnapsController < ApplicationController
     unviewed_usernames = split_param(:unviewed_usernames)
     viewed_sent_usernames = split_param(:viewed_sent_usernames)
     viewed_received_usernames = split_param(:viewed_received_usernames)
+    params[:trigger] = 'periodic_check' if params[:trigger].blank?
 
     unless current_user.mobile_notifier.pushes_enabled?
       unviewed_usernames.each do |username|
@@ -13,6 +14,18 @@ class SnapsController < ApplicationController
       current_user.email_notifier.notify_missed_received_snaps if viewed_received_usernames.present?
     end
 
+    increment_stories_metrics
+
     render_success
+  end
+
+
+  private
+
+  def increment_stories_metrics
+    count = params[:new_stories_count].to_i
+    trigger = params[:trigger]
+
+    StatsD.increment("stories.fetched.by_trigger.#{trigger}", count) if count > 0 && %w(periodic_check content_push).include?(trigger)
   end
 end
