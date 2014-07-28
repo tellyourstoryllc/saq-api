a154f71a258188f530e7c22d163cf67928431c4f
Brian Ray <brian.ray1@gmail.com>
2014-07-28 14:57:18 -0400

    Accept JSON true value for omit_sms_invite

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -145,7 +145,7 @@ class ApplicationController < ActionController::Base
   end
 
   def send_sms_invites?
-    params[:omit_sms_invite] != 'true' && !Settings.enabled?(:disable_sms_invites) && Bool.parse(current_user.sms_invites_allowed.value)
+    !Bool.parse(params[:omit_sms_invite]) && !Settings.enabled?(:disable_sms_invites) && Bool.parse(current_user.sms_invites_allowed.value)
   end
 
   def pagination_params
