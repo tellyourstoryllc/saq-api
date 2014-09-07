08e9f28358bccd4e6306de9b8a6bf13c062202be
Brian Ray <brian.ray1@gmail.com>
2014-09-07 11:53:41 -0400

    Add collegefootball to blacklist

--- app/models/user.rb
+++ app/models/user.rb
@@ -88,7 +88,7 @@ class User < ActiveRecord::Base
   delegate :registered, :registered?, to: :account
 
   COHORT_METRICS_TIME_ZONE = 'America/New_York'
-  BLACKLISTED_USERNAMES = %w(riolive2014 electriczoolive)
+  BLACKLISTED_USERNAMES = %w(riolive2014 electriczoolive collegefootball)
 
 
   def first_name
