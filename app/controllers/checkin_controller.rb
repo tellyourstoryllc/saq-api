faa1e7ba83f43a8794693c0beb8d525daf51a1d8
Brian Ray <brian.ray1@gmail.com>
2014-09-10 13:54:50 -0400

    Add blacklisted_usernames to checkin

--- app/controllers/checkin_controller.rb
+++ app/controllers/checkin_controller.rb
@@ -19,6 +19,7 @@ class CheckinController < ApplicationController
     client_config[:phone_verification_destination] = Rails.configuration.app['hook']['invite_from'] if params[:client] == 'ios'
     client_config[:client_version] = current_device.try(:client_version)
     client_config[:has_push_token] = current_device.try(:has_auth?)
+    client_config[:blacklisted_usernames] = Settings.get_list(:blacklisted_usernames)
 
     objects << client_config
 
