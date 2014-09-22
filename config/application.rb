f6f1878196a1fd5232219331dc098bdbb0cc5ae7
Brian Ray <brian.ray1@gmail.com>
2014-09-22 18:07:27 -0400

    Don't use asset pipeline
    Fixes stylesheets for admin console

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module KrazyChat
 
     # Disable Asset Pipeline
     config.assets.enabled = false
+    config.assets.compile = false
 
     config.generators do |g|
       g.test_framework :mini_test, spec: true, fixture: false
