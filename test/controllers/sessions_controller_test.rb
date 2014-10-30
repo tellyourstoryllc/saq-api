8ca58148cfcb2cb4fe424bb018d3a5a20dd20c74
Brian Ray <brian.ray1@gmail.com>
2014-10-30 16:01:48 -0400

    Fix tests from optimizations

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -25,7 +25,7 @@ describe SessionsController do
 
       result.size.must_equal 2
       result_must_include 'user', user.id, {'object_type' => 'user', 'id' => user.id, 'name' => user.name, 'username' => user.username,
-        'token' => user.token, 'status' => 'unavailable', 'idle_duration' => nil, 'status_text' => nil, 'client_type' => nil, 'avatar_url' => nil}
+        'token' => user.token, 'status' => nil, 'idle_duration' => nil, 'status_text' => nil, 'client_type' => nil, 'avatar_url' => nil}
       result_must_include 'account', account.id, {'object_type' => 'account', 'id' => account.id, 'user_id' => user.id,
         'one_to_one_wallpaper_url' => nil, 'facebook_id' => nil, 'time_zone' => 'America/New_York'}
     end
@@ -39,7 +39,7 @@ describe SessionsController do
 
       result.size.must_equal 2
       result_must_include 'user', user.id, {'object_type' => 'user', 'id' => user.id, 'name' => user.name, 'username' => user.username,
-        'token' => user.token, 'status' => 'unavailable', 'idle_duration' => nil, 'status_text' => nil, 'client_type' => nil, 'avatar_url' => nil}
+        'token' => user.token, 'status' => nil, 'idle_duration' => nil, 'status_text' => nil, 'client_type' => nil, 'avatar_url' => nil}
       result_must_include 'account', account.id, {'object_type' => 'account', 'id' => account.id, 'user_id' => user.id,
         'one_to_one_wallpaper_url' => nil, 'facebook_id' => nil, 'time_zone' => 'America/New_York'}
     end
@@ -57,7 +57,7 @@ describe SessionsController do
 
         result.size.must_equal 2
         result_must_include 'user', user.id, {'object_type' => 'user', 'id' => user.id, 'name' => user.name, 'username' => user.username,
-          'token' => user.token, 'status' => 'unavailable', 'idle_duration' => nil, 'status_text' => nil, 'client_type' => nil, 'avatar_url' => nil}
+          'token' => user.token, 'status' => nil, 'idle_duration' => nil, 'status_text' => nil, 'client_type' => nil, 'avatar_url' => nil}
         result_must_include 'account', account.id, {'object_type' => 'account', 'id' => account.id, 'user_id' => user.id,
           'one_to_one_wallpaper_url' => nil, 'facebook_id' => '100002345', 'time_zone' => 'America/New_York'}
       end
@@ -75,7 +75,7 @@ describe SessionsController do
 
     result.size.must_equal 2
     result_must_include 'user', user.id, {'object_type' => 'user', 'id' => user.id, 'name' => user.name, 'username' => user.username,
-      'token' => user.token, 'status' => 'unavailable', 'idle_duration' => nil, 'status_text' => nil, 'client_type' => nil, 'avatar_url' => nil}
+      'token' => user.token, 'status' => nil, 'idle_duration' => nil, 'status_text' => nil, 'client_type' => nil, 'avatar_url' => nil}
     result_must_include 'account', account.id, {'object_type' => 'account', 'id' => account.id, 'user_id' => user.id,
       'one_to_one_wallpaper_url' => nil, 'facebook_id' => nil, 'time_zone' => 'America/New_York', 'needs_password' => true}
   end
