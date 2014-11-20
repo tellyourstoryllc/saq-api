a40bbe4756d8461c581004ba423500d6e8f01557
Brian Ray <brian.ray1@gmail.com>
2014-11-20 14:04:28 -0500

    Disable Android completely till we’re off Snapchat

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 require 'active_record/validations'
 
 class ApplicationController < ActionController::Base
-  before_action :require_token, :set_locale, :create_or_update_device
+  before_action :disable_android, :require_token, :set_locale, :create_or_update_device
   around_action :set_client
   rescue_from ActiveRecord::RecordNotFound, Peanut::Redis::RecordNotFound, with: :render_404
   rescue_from Peanut::UnauthorizedError, with: :render_401
@@ -40,6 +40,10 @@ class ApplicationController < ActionController::Base
 
 
   private
+
+  def disable_android
+    render_error('Android is currently disabled.') if params[:client] == 'android'
+  end
 
   def require_token
     # Note: Android depends on this error message, so don't change it
