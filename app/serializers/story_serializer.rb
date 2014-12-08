928ac6ae04186b37fc0aad732046baec65639293
Brian Ray <brian.ray1@gmail.com>
2014-12-08 16:42:20 -0500

    Don’t need story.forwards_count

--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -1,8 +1,6 @@
 class StorySerializer < MessageSerializer
   attributes :snapchat_media_id, :comments_count, :comments_disabled
 
-  def forwards_count; 0 end
-
   def comments_count
     object.comment_ids.size
   end
