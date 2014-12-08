57d19122ee1fc1abba98902f9cb865434ca27d1d
Brian Ray <brian.ray1@gmail.com>
2014-12-08 16:42:38 -0500

    Don’t need comments attrs for story

--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -1,11 +1,3 @@
 class StorySerializer < MessageSerializer
-  attributes :snapchat_media_id, :comments_count, :comments_disabled
-
-  def comments_count
-    object.comment_ids.size
-  end
-
-  def comments_disabled
-    object.comments_disabled?
-  end
+  attributes :snapchat_media_id
 end
