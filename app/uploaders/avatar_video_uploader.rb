a2f13d574dfdeb8942b07920fa222b842fb05d3c
Brian Ray <brian.ray1@gmail.com>
2014-12-22 12:52:05 -0500

    Don’t create moderation thumbnails on creation
    We only need the moderation thumbnails if it gets flagged

--- app/uploaders/avatar_video_uploader.rb
+++ app/uploaders/avatar_video_uploader.rb
@@ -3,28 +3,28 @@
 class AvatarVideoUploader < BaseUploader
   include Peanut::AnimatedGif
 
-  version :thumbnails do
-    process :recreate_thumbnails
-  end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  def extension_white_list
-    %w(mp4)
-  end
-
-  def recreate_thumbnails
-    # If there were pre-existing thumbnails, destroy them.
-    model.thumbnails.destroy_all
-
-    # Get the length of the video.
-    movie = FFMPEG::Movie.new(current_path)
-    duration_in_seconds = movie.duration
-
-    # Create new thumbnails.
-    (0 .. duration_in_seconds.to_i).each do |i|
-      # Give it the video file.  It will convert it to an image based on the
-      # offset.
-      VideoThumbnail.create!(video: model, offset: i, image: file)
-    end
-  end
+#  version :thumbnails, {if: :flagged?} do
+#    process :recreate_thumbnails
+#  end
+#
+#  # Add a white list of extensions which are allowed to be uploaded.
+#  def extension_white_list
+#    %w(mp4)
+#  end
+#
+#  def recreate_thumbnails
+#    # If there were pre-existing thumbnails, destroy them.
+#    model.thumbnails.destroy_all
+#
+#    # Get the length of the video.
+#    movie = FFMPEG::Movie.new(current_path)
+#    duration_in_seconds = movie.duration
+#
+#    # Create new thumbnails.
+#    (0 .. duration_in_seconds.to_i).each do |i|
+#      # Give it the video file.  It will convert it to an image based on the
+#      # offset.
+#      VideoThumbnail.create!(video: model, offset: i, image: file)
+#    end
+#  end
 end
