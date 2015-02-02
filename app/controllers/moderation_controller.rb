79e623a085e41cc82ecbdfe1a1c008f1886fed0e
Brian Ray <brian.ray1@gmail.com>
2015-02-02 18:07:35 -0500

    Create a VideoRejection record when censored

--- app/controllers/moderation_controller.rb
+++ app/controllers/moderation_controller.rb
@@ -5,7 +5,7 @@ class ModerationController < ApplicationController
   def callback
     # Try to find the given ActiveRecord or Redis object
     model_class = params[:model_class].to_s.constantize
-    model = if params[:model_id].present?
+    @model = if params[:model_id].present?
               if model_class.respond_to?(:find)
                 model_class.find(params[:model_id])
               elsif model_class.ancestors.include?(Peanut::RedisModel)
@@ -13,12 +13,19 @@ class ModerationController < ApplicationController
               end
             end
 
-    if model
-      if params[:passed].try(:include?, 'nudity')
-        model.approve!
-      elsif params[:failed].try(:include?, 'nudity')
-        model.censor!
+    if @model
+      if params[:passed] && params[:passed].include?('video_approval') && @model.is_a?(Story)
+        @model.approve!
+      elsif params[:failed] && params[:failed].include?('video_approval') && @model.is_a?(Story)
+        video_rejection = VideoRejection.create! do |vr|
+          vr.story_id = @model.id
+          vr.video_moderation_reject_reason_id = params[:reject_reason_id]
+          vr.custom_message_to_user = params[:message_to_user]
+        end
+
+        @model.censor!
       end
+
       render_success
     else
       render_error("Could not find a record for the given model_class and model_id.")
