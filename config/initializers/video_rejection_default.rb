d0f008c0dcc6f6a691184c024aaae080540b2cdb
Brian Ray <brian.ray1@gmail.com>
2015-02-02 18:34:50 -0500

    Don't require default for test

--- config/initializers/video_rejection_default.rb
+++ config/initializers/video_rejection_default.rb
@@ -1,1 +1,1 @@
-raise 'Missing VideoModerationRejectReason default record.' unless VideoModerationRejectReason.find_default.present?
+raise 'Missing VideoModerationRejectReason default record.' unless Rails.env.test? || VideoModerationRejectReason.find_default.present?
