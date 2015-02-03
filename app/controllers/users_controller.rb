7b66d7671cf09a46ed53cbf33b5aefcfbc7cdf99
Brian Ray <brian.ray1@gmail.com>
2015-02-03 12:10:49 -0500

    Register an existing user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,13 +39,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    @invite = Invite.find_by(invite_token: params[:invite_token]) if params[:invite_token].present?
-    @current_user = @invite.try(:recipient)
-    @current_user ||= User.find_by(username: user_params[:username])
-    @account = @current_user.try(:account)
+    # First attempt to find an existing user by the given user token
+    if current_user
+      @account = current_user.account
 
-    # If an invite_token is included, OR if the given username already exists but that account hasn't
-    # yet been 'claimed' by registering, just update that user
+    # If not found, try to find a user by the given invite token
+    else
+      @invite = Invite.find_by(invite_token: params[:invite_token]) if params[:invite_token].present?
+      @current_user = @invite.try(:recipient)
+      @current_user ||= User.find_by(username: user_params[:username])
+      @account = @current_user.try(:account)
+    end
+
+    # If a user was found by a user token or invite token and he's not yet registered, just update that user
     if @current_user && @account && !@account.registered?
       @current_user.update!(user_params)
       attrs = account_params
