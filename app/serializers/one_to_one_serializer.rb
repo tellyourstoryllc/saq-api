663bea020cf57f4939f7e8a60922b2da0810ba0d
Brian Ray <brian.ray1@gmail.com>
2015-03-18 16:07:20 -0400

    Return one_to_one.pending

--- app/serializers/one_to_one_serializer.rb
+++ app/serializers/one_to_one_serializer.rb
@@ -1,6 +1,6 @@
 class OneToOneSerializer < ActiveModel::Serializer
   attributes :object_type, :id, :member_ids, :messages_count, :last_message_at,
-    :last_seen_rank, :last_deleted_rank, :hidden
+    :last_seen_rank, :last_deleted_rank, :hidden, :pending
 
   def messages_count
     object.fetched_message_ids_count || object.message_ids.size
@@ -8,5 +8,9 @@ class OneToOneSerializer < ActiveModel::Serializer
 
   def member_ids
     object.fetched_member_ids
+  end
+
+  def pending
+    object.pending?(scope)
   end
 end
